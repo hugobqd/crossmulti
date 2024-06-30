4e006f010fc4631bdf7b1ba463ec5fe9eb3274a5
Hugo <hugobqd@gmail.com>
2024-06-30 08:51:48 +0200

    layout

--- components/main/Frame.vue
+++ components/main/Frame.vue
@@ -14,33 +14,37 @@ watch([a, b, c], () => {
 });
 </script>
 <template>
-  <h1 class="text-center">Cross multiplication</h1>
-  <div class="grid grid-cols-2 max-w-sm mx-auto">
-    <input
-      v-model="a"
-      type="number"
-      class="border border-gray-300 p-2 m-2"
-      placeholder="a"
-    />
-    <input
-      v-model="b"
-      type="number"
-      class="border border-gray-300 p-2 m-2"
-      placeholder="b"
-    />
-    <input
-      v-model="c"
-      type="number"
-      class="border border-gray-300 p-2 m-2"
-      placeholder="c"
-    />
-    <input
-      v-model="d"
-      type="number"
-      class="border border-gray-300 p-2 m-2"
-      placeholder="d"
-      disabled
-    />
+  <div class="p-10 min-h-svh bg-cyan-100 place-items-center grid">
+    <section class="flex flex-col gap-6">
+      <h1 class="text-center">Cross multiplication</h1>
+      <div class="grid grid-cols-2 max-w-sm mx-auto">
+        <input
+          v-model="a"
+          type="number"
+          class="border border-gray-300 p-2 m-2"
+          placeholder="a"
+        />
+        <input
+          v-model="b"
+          type="number"
+          class="border border-gray-300 p-2 m-2"
+          placeholder="b"
+        />
+        <input
+          v-model="c"
+          type="number"
+          class="border border-gray-300 p-2 m-2"
+          placeholder="c"
+        />
+        <input
+          v-model="d"
+          type="number"
+          class="border border-gray-300 p-2 m-2"
+          placeholder="d"
+          disabled
+        />
+      </div>
+      <pre v-if="!crossMultiIsValid" class="text-red-500">Ohoh! problem!</pre>
+    </section>
   </div>
-  <pre>crossMultiIsValid :{{ crossMultiIsValid ? "valid" : "no" }}</pre>
 </template>
