6c7ea89c53b9140979b3c545e5a52e77d373c3a6
Hugo <hugobqd@gmail.com>
2024-06-30 06:16:36 +0200

    tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Frame />
   </div>
 </template>
---
+++ components/Frame.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-center">Cross multiplication</h1>
+</template>
