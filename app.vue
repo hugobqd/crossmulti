bdb5495826b72dc15a6a9f0125a19624541df652
Hugo <hugobqd@gmail.com>
2024-06-30 06:49:41 +0200

    title

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: "Cross Multi",
+  meta: [
+    { name: "Cross multiplication", content: "Cross multiplication easily" },
+  ],
+});
+</script>
 <template>
   <div>
     <MainFrame />
