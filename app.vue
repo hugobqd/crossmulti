c379746bd9e261d24890d185fd330f48881067b3
Hugo <hugobqd@gmail.com>
2024-06-30 07:01:19 +0200

    CM1 Init project (#1)
    * init Nuxt
    * tailwindcss
    * typescript
    * eslint
    * run lint
    * prettier
    * title
    * npm update

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+useHead({
+  title: "Cross Multi",
+  meta: [
+    { name: "Cross multiplication", content: "Cross multiplication easily" },
+  ],
+});
+</script>
+<template>
+  <div>
+    <MainFrame />
+  </div>
+</template>
